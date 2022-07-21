20dca868af190dab7330de3c3388ac4cf8201564
李春阳 <lichunyang@tinman.cn>
2022-07-21 16:20:34 +0800

    fix: next export img

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   assetPrefix: 'http://xuxiaohua.xhcy.online:8088',
+  experimental: {
+    images: {
+        unoptimized: true
+    }
+}
 }
 
 module.exports = nextConfig
